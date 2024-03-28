0fbe54936d181d0f563f9e3f2e9cd9962cf03960
kahouechi <uechikaho@gmail.com>
2024-03-28 11:20:31 -0400

    working on header

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
-import Header from './components/Header/Header'
+import Header from './components/Header/Header.jsx'
 import {Outlet} from 'react-router-dom'
-import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer.jsx'
 
 const Layout = () => {
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
+    <section className='h-wrapper'>
+      <div className='h-container'>
+        <Link to="/">
+          <div className='title'>
+            <h1>Kaho's Portfolio!!</h1>
+          </div>
+        </Link>
+
+        <div className='h-menu'>
+          <Link to="/projects">PROJECTS</Link>
+          <Link to="/resume">RESUME</Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
