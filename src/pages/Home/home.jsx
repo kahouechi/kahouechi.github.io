1407203c936420fd61f6c94f261fd180eee3fe56
kahouechi <uechikaho@gmail.com>
2024-03-31 15:43:01 -0400

    adding an about me section

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero.jsx';
+import AboutMe from '../../components/AboutMe/AboutMe.jsx';
 
 const Home = () => {
   return (
     <section className="home">
       <Hero />
+      <AboutMe />
     </section>
   )
 }
