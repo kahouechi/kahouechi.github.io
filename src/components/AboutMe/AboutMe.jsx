7404f467e6d3e71cb01f01fe2f0a6c90aab3877f
kahouechi <uechikaho@gmail.com>
2024-03-31 17:13:12 -0400

    more work on the about me section

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './AboutMe.css'
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <section className="am-wrapper">
+      <div className="am-container">
+        <div className="am-text">
+          <h1>Get to Know Me!</h1>
+          <p>
+            Hi! My name is Kaho &#40;pronounced kah-ho&#41;. I am a 16 year old high school student, interested in programming!
+          </p>
+        </div>
+        <div className="image">
+          <img /> 
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,29 @@ import './Footer.css'
 const Footer = () => {
   return (
     <section className="f-wrapper">
-      hello
+
+      <div className="f-container">
+        <div className='f-title'>
+          <span>
+            Thank you for reading through my website!
+          </span>
+        </div>
+
+        <div className='flex-container'>
+          <div className="f-left">
+            <h1>Contact</h1>
+            <span>uechikaho@gmail.com</span>
+            <span>647-674-5312</span>
+          </div>
+
+          <div className="f-right">
+            <h1>Socials</h1>
+            <span>
+              Check out my socials below!
+            </span>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
