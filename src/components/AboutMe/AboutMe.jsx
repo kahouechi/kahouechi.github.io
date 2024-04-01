896bb4fd12a91b027d4532983d7a89acd09b4774
kahouechi <uechikaho@gmail.com>
2024-03-31 21:20:23 -0400

    work on about me

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -6,10 +6,20 @@ const AboutMe = () => {
     <section className="am-wrapper">
       <div className="am-container">
         <div className="am-text">
-          <h1>GET TO KNOW ME!</h1>
+          <h1>GET TO KNOW ME :&#41;</h1>
           <p>
-            Hi! My name is Kaho &#40;pronounced kah-ho&#41; and I am a 16 year old high school student. I like working on coding projects and building on my knowledge of web development and competitve programming!
+            Hi! My name is Kaho &#40;pronounced kah-ho&#41; and I am a 16 year old high school student.
           </p>
+          <p>
+            I like working on coding projects and building on my knowledge of web development and competitve programming!
+          </p>
+        </div>
+
+        <div className='fun-facts'>
+          <div className='fun-fact'>
+            <h1>I like fashion!</h1>
+            <p>ksdnfnfkandfn</p>
+          </div>
         </div>
       </div>
     </section>
