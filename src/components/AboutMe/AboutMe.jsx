5a3143eeda3327b31a06ae8f204b04ef0a0a1e68
kahouechi <uechikaho@gmail.com>
2024-04-01 12:46:44 -0400

    more styling + adding a contact section

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -9,7 +9,6 @@ const AboutMe = () => {
           <h1>GET TO KNOW ME :&#41;</h1>
           <p>
             Hi! My name is Kaho &#40;pronounced kah-ho&#41; and I am a 16 year old high school student.
-            I like working on coding projects and building on my knowledge of web development and competitve programming!
           </p>
         </div>
 
@@ -28,9 +27,27 @@ const AboutMe = () => {
           </div>
         </div>
 
-        <div className="projects-container">
-          <h1>CHECK OUT MY PROJECTS!</h1>
+        <div className="projects-wrapper">
+          <div className="projects-container">
+            <div className="projects-left">
+              image
+            </div>
+            <div className="projects-right">
+              <h1>CHECK OUT MY PROJECTS!!</h1>
+              <p>
+                I like working on coding projects and building on my knowledge of web development and competitve programming! <br /> -- Check out my work here
+              </p>
+            </div>
+          </div>
+        </div>
 
+        <div className="contact-wrapper">
+          <div className="contact-container">
+            <h1>CONTACT ME</h1>
+            <p>
+              I am always up to start a conversation! Feel free to contact me through any of my socials, and I will get back to you as soon as I can.
+            </p>
+          </div>
         </div>
       </div>
     </section>
