4a9eaca221014429daedf00f7c4b515c4690bfa1
kahouechi <uechikaho@gmail.com>
2024-04-01 16:36:48 -0400

    more about me

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -14,19 +14,37 @@ const AboutMe = () => {
 
         <div className='fun-facts'>
           <div className='fun-fact'>
-            <h1>I like being creative!</h1>
-            <p>I like to dabble in fashion, music, and art... <br /> 
-            <br />
-            I love ALLLLLL genres of music, but my current favourite artists are Childish Gambino, MF DOOM, and Sade!!
+            <h1>I was born in Japan!</h1>
+            <p>
+              &#9733;	I was born in Okinawa, Japan, but I moved to Toronto, Canada when I was four years old
             </p>
           </div>
+
           <div className='fun-fact'>
-            <h1>I was born in Japan!</h1>
-            <p>I was born in Okinawa, Japan, but I moved to Toronto, Canada when I was four years old!</p>
+            <h1>I like to dabble in fashion and music...</h1>
+            <p>
+              &#9733;	I like exploring different clothing pieces and ways to style them
+              <br/>
+              <br/>
+              &#9733;	I love ALLLLLL genres of music, but my current favourite artists are Childish Gambino, MF DOOM, and Sade!!
+            </p>
           </div>
+
           <div className='fun-fact'>
-            <h1>I'm interested in sustainability!</h1>
-            <p>des</p>
+            <h1>I loovveee food...</h1>
+            <p>
+              &#9733;	There's nothing better than a good meal or sweet treat :P
+            </p>
+          </div>
+          
+          <div className='fun-fact'>
+            <h1>I like nature!</h1>
+            <p>
+              &#9733; I love being in nature, especially sunsets!! So beautiful &#x266C;
+              <br/>
+              <br/>
+              &#9733;	Other than my interest for coding, I like to learn about sustainability. I am currently working on creating a new Youth Climate Action Engagement Strategy as part of the Toronto Schools Youth Climate Leadership Group!
+            </p>
           </div>
         </div>
       </div>
