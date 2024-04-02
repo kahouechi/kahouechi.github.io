e7d9eaa8d3db884d5ea7c7b473f5639e330498d7
kahouechi <uechikaho@gmail.com>
2024-04-02 11:04:34 -0400

    styling project previews + adding a link to github

--- src/pages/Projects/projects.jsx
+++ src/pages/Projects/projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './projects.css'
 import ProjectsHero from '../../components/Hero/ProjectsHero.jsx'
 import data from '../../data/projects.json'
+import { MdOpenInNew } from "react-icons/md";
 
 const Projects = () => {
   return (
@@ -12,12 +13,18 @@ const Projects = () => {
           {
             data.map((project, i) => (
               <div className="project">
-                <div className="project-top">
-                  <h1>{project.heading}</h1>
+                <div className="project-top pb-5">
+                  <h1 className='my-1'>{project.heading}</h1>
                   <h2>{project.subheading}</h2>
                 </div>
                 <img src={project.image}/>
-                <p>{project.description}</p>
+                <p className='mb-5'>{project.description}</p>
+                <div className="view-more-button">
+                  <MdOpenInNew /> 
+                  <a target="_blank" href={project.link} className='view-more'>
+                    View Project
+                  </a>
+                </div>
               </div>
             ))
           }
