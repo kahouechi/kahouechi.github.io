7ecd1d54ed7e861969e0a615b23985460826dd7d
kahouechi <uechikaho@gmail.com>
2024-04-02 10:20:02 -0400

    added to projects page + styled fun facts a bit more

--- src/pages/Projects/projects.jsx
+++ src/pages/Projects/projects.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import './projects.css'
 import ProjectsHero from '../../components/Hero/ProjectsHero.jsx'
+import data from '../../data/projects.json'
 
 const Projects = () => {
   return (
     <section className="projects">
       <ProjectsHero />
       <div className="page">
-        
+        <div className="projects-container">
+          {
+            data.map((project, i) => (
+              <div className="project">
+                <h1>{project.heading}</h1>
+                <h2>{project.subheading}</h2>
+                <img src={project.image}/>
+                <p>{project.description}</p>
+              </div>
+            ))
+          }
+        </div>
       </div>
     </section>
   )
