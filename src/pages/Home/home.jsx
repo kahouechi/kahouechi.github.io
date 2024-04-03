2830011397a180404f60b439a8d31ba82adfda3e
kahouechi <uechikaho@gmail.com>
2024-04-03 09:32:42 -0400

    taking out a non-existing component

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -4,7 +4,6 @@ import Hero from '../../components/Hero/HomeHero.jsx';
 import AboutMe from '../../components/AboutMe/AboutMe.jsx';
 import ProjectsHome from '../../components/ProjectsHome/ProjectsHome.jsx';
 import ContactHome from '../../components/ContactHome/ContactHome.jsx'
-import SeperationLine from '../../components/SeparationLine/SeperationLine.jsx';
 
 const Home = () => {
   return (
