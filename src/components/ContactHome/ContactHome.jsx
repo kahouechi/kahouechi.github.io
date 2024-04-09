ee0c2be57fa0582baaae1b1c03c5bd197f1c8b71
kahouechi <uechikaho@gmail.com>
2024-04-09 09:38:20 -0400

    fixing stuff

--- src/components/ContactHome/ContactHome.jsx
+++ src/components/ContactHome/ContactHome.jsx
@@ -2,14 +2,46 @@ import React from 'react'
 import './ContactHome.css'
 
 const ContactHome = () => {
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "5df5f7e3-28f0-4ac5-a808-bf67fe0a7c44");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+    }
+  };
+
   return (
-    <div className="contact-wrapper">
-      <div className="contact-container">
+    <div className="contact-container">
+      <div className="contact-title">
         <h1>CONTACT ME</h1>
         <p>
-          I am always up to start a conversation! Feel free to contact me through any of my socials, and I will get back to you as soon as I can.
+          I am always up to start a conversation! Feel free to contact me through this form or any of my socials, and I will get back to you as soon as I can.
         </p>
       </div>
+
+      <form onSubmit={onSubmit} className='form-container'>
+        <input type="text" name="name" placeholder='Your Name' className='contact-input' required/>
+        <input type="email" name="email" placeholder='Your Email' className='contact-input' required/>
+        <textarea name="message" placeholder='Your Message' className='contact-input' required></textarea>
+        <input type="hidden" name="redirect" value="https://web3forms.com/success"/>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
           <Link to="/">HOME</Link>
           <Link to="/projects">PROJECTS</Link>
           <Link to="/resume">RESUME</Link>
-          <Link to="/contact">CONTACT</Link>
+          <Link to="/">CONTACT</Link>
         </div>
       </div>
     </section>
