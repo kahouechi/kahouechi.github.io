85953a918d6e21d20c8aeaede91dcffab8d6e5b0
kahouechi <uechikaho@gmail.com>
2024-04-14 23:32:32 -0400

    adding contact info

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,16 +1,54 @@
 import React from 'react'
 import './Contacts.css'
 import { MdOutlineEmail } from "react-icons/md"
+import { FaPhoneFlip } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 
 const Contacts = () => {
   return (
     <section className="contacts">
       <div className='contacts-wrapper'>
         <div className='contacts-container'>
-          <div className='contact'>
-            <MdOutlineEmail/>
-            <h1>Email me!</h1>
+          <div className='contact-info'>
+            <div className='contact-info-left'>
+              <MdOutlineEmail/>
+            </div>
+            <div className='contact-info-right'>
+              <h1 className='m-1'>Email</h1>
+              <p>uechikaho@gmail.com</p>
+            </div>
+          </div>
 
+          <div className='contact-info'>
+            <div className='contact-info-left'>
+              <FaPhoneFlip/>
+            </div>
+            <div className='contact-info-right'>
+              <h1 className='m-1'>Phone</h1>
+              <p>647-674-5312</p>
+            </div>
+          </div>
+
+          <div className='contact-info'>
+            <div className='contact-info-left'>
+              <FaGithub/>
+            </div>
+            <div className='contact-info-right'>
+              <h1 className='m-1'>GitHub</h1>
+              <p>@kahouechi</p>
+            </div>
+          </div>
+
+          <div className='contact-info'>
+            <div className='contact-info-left'>
+              <FaLinkedin/>
+            </div>
+            <div className='contact-info-right'>
+              <h1 className='m-1'>LinkedIn</h1>
+              <p>linkedin.com</p>
+            </div>
           </div>
         </div>
       </div>
