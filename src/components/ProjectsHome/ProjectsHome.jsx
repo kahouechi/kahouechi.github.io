b6a6ba7ee88f89ac29e22d938d138c53907bc370
kahouechi <uechikaho@gmail.com>
2024-04-18 10:28:56 -0400

    adding media queries

--- src/components/ProjectsHome/ProjectsHome.jsx
+++ src/components/ProjectsHome/ProjectsHome.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import './ProjectsHome.css'
 import { Link } from 'react-router-dom'
-import { FaArrowLeftLong } from "react-icons/fa6"
+import { FaArrowRight } from "react-icons/fa";
 import data from '../../data/projects.json'
 import ProjectCarousel from '../ProjectCarousel/ProjectCarousel.jsx'
 
 
 const ProjectsHome = () => {
   return (
-    <div className="projects-wrapper">
-      <div className="projects-container">
-        <div className="projects-left">
-          <ProjectCarousel data={data}/>
-        </div>
-        <div className="projects-right">
+    <div className="projects-home-wrapper">
+      <div className="projects-home-container">
+        <div className="projects-home-text">
           <h1>CHECK OUT MY PROJECTS!!</h1>
           <p>
             I like working on coding projects and building on my knowledge of web development and competitve programming!
           </p>
           <br/>
           <Link to="/projects" className='see-more-link'>
-            <FaArrowLeftLong />
             <span>See my work here</span>
+            <FaArrowRight />
           </Link>
+        </div>
+        <div className="projects-home-carousel">
+          <ProjectCarousel data={data}/>
         </div>
       </div>
     </div>
