40df917780ce110bec0a78de424c422b28af4aa4
kahouechi <uechikaho@gmail.com>
2024-04-18 16:36:13 -0400

    fixing hero on mobile

--- src/components/Hero/HomeHero.jsx
+++ src/components/Hero/HomeHero.jsx
@@ -5,12 +5,10 @@ const Hero = () => {
   return (
     <section className="hero-wrapper bg-gradient-to-tr from-violet-950 via-slate-900 to-indigo-950">
       <div className="hero-container">
-        <div className="hero-left">
-          <div className="hero-title">
-            <p>Hello! I'm</p>
-            <h1>KAHO UECHI</h1>
-            <p>she/her</p>
-          </div>
+        <div className="hero-text">
+          <h2>Hello! I'm</h2>
+          <h1>KAHO UECHI</h1>
+          <p>she/her</p>
         </div>
       </div>
     </section>
--- src/components/Hero/ProjectsHero.jsx
+++ src/components/Hero/ProjectsHero.jsx
@@ -5,13 +5,9 @@ const ProjectsHero = () => {
   return (
     <section className="hero-wrapper bg-gradient-to-bl from-sky-900 via-slate-900 to-blue-900">
       <div className="hero-container">
-        <div className="hero-left">
-          <div className="hero-title">
-            <h1>PROJECTS</h1>
-            <p>Here are some of the projects I have made, applying my knowledge of different technologies.
-            <br /> 
-            All of my projects are unfinished at the moment, but I am working on them!</p>
-          </div>
+        <div className="hero-text">
+          <h1>PROJECTS</h1>
+          <p>Here are some of the projects I have made, applying my knowledge of different technologies. All of my projects are unfinished at the moment, but I am working on them!</p>
         </div>
       </div>
     </section>
