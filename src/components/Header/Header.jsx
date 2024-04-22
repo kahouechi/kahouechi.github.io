47ecec76f79c161e3e0c8f890180e0173bc7751e
kahouechi <uechikaho@gmail.com>
2024-04-22 10:46:06 -0400

    made pop up navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,6 +19,12 @@ const Header = () => {
 
   const [menuOpened, setMenuOpened] = useState(false);
 
+  const getMenuStyles = (menuOpened) => {
+    if (document.documentElement.clientWidth <= 800) {
+      return {right: !menuOpened && "-100%"}
+    }
+  }
+
   return (
     <section className={color ? 'h-wrapper h-scroll' : 'h-wrapper'}>
       <div className='h-container'>
@@ -28,14 +34,16 @@ const Header = () => {
           </div>
         </Link>
 
-        <div className='h-menu'>
+        <div className='h-menu'
+          style={getMenuStyles(menuOpened)}
+          onClick={() => setMenuOpened((prev)=>!prev)}>
           <Link to="/" onClick={scrollToTop}>HOME</Link>
           <Link to="/projects" onClick={scrollToTop}>PROJECTS</Link>
           {/* <Link to="/resume">RESUME</Link> */}
           <Link to="/contact" onClick={scrollToTop}>CONTACT</Link>
         </div>
       </div>
-      <div className='mobile-button'>
+      <div className='mobile-button' onClick={() => setMenuOpened((prev)=>!prev)}>
         <IoMdMenu size={25} />
       </div>
     </section>
