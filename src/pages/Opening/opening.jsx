a2c0bde6451c05705008f65f61a09c24d1355230
kahouechi <uechikaho@gmail.com>
2024-05-01 01:16:01 -0400

    delaying name transition

--- src/pages/Opening/opening.jsx
+++ src/pages/Opening/opening.jsx
@@ -28,6 +28,7 @@ const Opening = () => {
             animate={{scale: 1, opacity: 1 }}
             transition={{
               duration: 1,
+              delay: 0.3,
               type: "spring"
             }}
             className="m-8"
